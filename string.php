2b26da392d7011657d450a89c9f1f14c4171b01e
Abdullah Anaman <42936448+berrydeblaq@users.noreply.github.com>
2021-02-08 20:43:37 +0000

    Create string.php

---
+++ string.php
@@ -0,0 +1,23 @@
+<?php
+
+// //strlen()
+//     $name = "Abdullah Anaman";
+//     echo strlen($name);
+
+//str_word_count()
+// $name = "Abdullah Anaman";
+//    echo str_word_count($name);
+
+//strrev()
+// $name = "Abdullah Anaman";
+//    echo strrev($name);
+
+//strpos()
+// $name = "Abdullah Anaman";
+// echo strpos($name, 'Anaman');
+
+// str_replace()
+$name = "Abdullah Anaman";
+echo str_replace('Abdullah', 'Salamatu', $name);
+
+?>
