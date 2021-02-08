e0ccc38e58a643e93dd0f35ddb937f16f5290289
Abdullah Anaman <42936448+berrydeblaq@users.noreply.github.com>
2021-02-08 21:01:02 +0000

    Create switch.php

---
+++ switch.php
@@ -0,0 +1,20 @@
+<?php
+
+//$color = 'white';
+$color = 'blue';
+
+switch($color)
+{
+    case "red":
+        echo "color is red";
+        break;
+    case "black":
+        echo "color is black";
+        break;
+    case "blue":
+        echo "color is blue";
+        break;
+    default:
+        echo "color not found";
+}
+?>
