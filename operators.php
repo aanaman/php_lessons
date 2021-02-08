640c50b9db1a0df45a9baeca29741a9e5773077a
Abdullah Anaman <42936448+berrydeblaq@users.noreply.github.com>
2021-02-08 21:39:39 +0000

    Create operators.php

---
+++ operators.php
@@ -0,0 +1,47 @@
+<?php
+//assignment operators
+$x = 29;
+$y = 20;
+
+
+//arithmetic operators
+//echo ($x+$y);
+
+//comparison operators
+// if($x==29)
+// {
+//     echo "this is a comparison operator";
+// }
+
+// if($x!=$y)
+// {
+//     echo "this is a comparison operator";
+// }
+// else
+// {
+//     echo "x is equal to y";
+// }
+
+//increment operator 
+// $x++;
+// echo $x;
+
+//logical operator
+// if($x==29 AND $x!="")
+// {
+//     echo "x ix 29";
+// }
+
+// else
+// {
+//     echo "x is not 29";
+// }
+
+
+//spaceship operator
+echo 1<=>2, "<br/>";
+echo 3<=>5, "<br/>";
+echo 15<=>15;
+
+
+?>
